cc8ae7347fcb52294150c709b458292dbb1b5266
Sharad Sisodiya <92092443+Sharad-18@users.noreply.github.com>
2024-02-16 09:40:48 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Capstone Project 👋")
+st.write("In my ambitious project, I have embarked on a mission to revolutionize the real estate experience by harnessing the power of machine learning. The central focus of this endeavor is the prediction of house and flat prices, an invaluable tool for both buyers and sellers. Within this project, I have meticulously designed and deployed three distinct models, all seamlessly integrated into the same website to provide a holistic and user-friendly platform \n The cornerstone of this project is the Prediction Model, a sophisticated machine learning algorithm that leverages historical data and market indicators to accurately forecast housing prices. By analyzing a myriad of variables such as location, property size, amenities, and market trends, users can make informed decisions about their investments with confidence.")
+
+st.sidebar.success("Select a demo above.")
